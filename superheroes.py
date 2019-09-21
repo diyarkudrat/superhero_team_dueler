318bf2263bdb4ea6b04a415f7fad3b4b46484c6a
Diyar Kudrat <dkudrat@gmail.com>
2019-09-20 20:12:48 -0700

    added remaining methods in Hero class. Determines winner of a battle.

--- superheroes.py
+++ superheroes.py
@@ -46,16 +46,31 @@ class Hero:
 
     def add_armor(self, armor):
         return self.armors.append(armor)
-        
 
+    def defend(self):
+        total_block = 0
+        for armor in self.armors:
+            total_block += armor.block()
+        return total_block
 
+    def take_damage(self, damage):
+        after_damage = damage - self.defend()
+        self.current_health -= after_damage
+        return self.current_health
 
+    def is_alive(self):
+        if self.current_health > 0:
+            return True
+        else:
+            return False
 
+    def fight(self, opponent):
+        while self.is_alive() == True and opponent.is_alive() == True:
+            self.take_damage(opponent.attack)
+            opponent.take_damage(self.attack)
 
-
-
-
-
+            if self.is_alive == False or opponent.is_alive == False:
+                print(self.name)
 
 
 
@@ -76,9 +91,34 @@ if __name__ == '__main__':
     # hero.add_ability(ability)
     # print(hero.abilities)
 
-    ability = Ability("Great Debugging", 50)
-    another_ability = Ability("Smarty Pants", 90)
-    hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    hero.add_ability(another_ability)
-    print(hero.attack())
+    # ability = Ability("Great Debugging", 50)
+    # another_ability = Ability("Smarty Pants", 90)
+    # hero = Hero("Grace Hopper", 200)
+    # hero.add_ability(ability)
+    # hero.add_ability(another_ability)
+    # print(hero.attack())
+
+    #test code for take_damage method
+    # hero = Hero("Grace Hopper", 200)
+    # shield = Armor("Shield", 50)
+    # hero.add_armor(shield)
+    # hero.take_damage(50)
+    # print(hero.current_health)
+
+    # hero = Hero("Grace Hopper", 200)
+    # hero.take_damage(150)
+    # print(hero.is_alive())
+    # hero.take_damage(15000)
+    # print(hero.is_alive())
+
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
