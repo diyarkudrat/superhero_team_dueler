da0f9b6db395677c2d065e6a25de0bd6f04355f8
Diyar Kudrat <dkudrat@gmail.com>
2019-09-20 18:43:53 -0700

    added methods in a new Hero class

--- superheroes.py
+++ superheroes.py
@@ -3,18 +3,82 @@ import random
 
 
 class Ability:
-    def __init__(self, name, x):
+    def __init__(self, name, max_damage):
         self.name = name
-        self.max_damage = x
+        self.max_damage = max_damage
 
     def attack(self):
         attack_damage = random.randint(0, self.max_damage)
         return attack_damage
 
+class Armor:
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        block_strength = random.randint(0, self.max_block)
+        return block_strength
+
+class Hero:
+    def __init__(self, name, start_health = 100):
+        self.name = name
+        self.start_health = start_health
+        self.current_health = start_health
+        self.abilities = []
+        self.armors = []
+
+    # add_ability: Parameters: ability:Ability Object
+    # attack: No Parameters
+    # defend: incoming_damage: Integer
+    # take_damage: Parameters: damage
+    # is_alive: No Parameters
+    # fight: Parameters: opponent: Hero Class
+
+    def add_ability(self, ability):
+        return self.abilities.append(ability)
+
+    def attack(self):
+        total = 0
+        for ability in self.abilities:
+            total += ability.attack()
+        return total
+
+    def add_armor(self, armor):
+        return self.armors.append(armor)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 
-    hero_ability = Ability('Hulk', 55)
-    print(hero_ability.name)
+    # hero_ability = Hero('Hulk', 100, 100)
+    # print(hero_ability.name)
     # print(hero_ability.max_damage)
-    print(hero_ability.attack())
+    # print(hero_ability.attack())
+    # print(hero_ability.block())
+    # print(hero_ability.present_health())
+
+    # ability = Ability("Great Debugging", 50)
+    # hero = Hero("Grace Hopper", 200)
+    # hero.add_ability(ability)
+    # print(hero.abilities)
+
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
